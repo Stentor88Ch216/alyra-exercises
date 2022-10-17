828e6d6b9b324773c15b6d0e5a517399a473b6c6
Thomas Dch <33961528+Stentor88Ch216@users.noreply.github.com>
2022-10-17 23:15:05 +0200

    Update voting-project.sol

--- voting-project.sol
+++ voting-project.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.7.0 <0.9.0;
 
-
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 /*
